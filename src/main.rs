cc7da4b090f75f2bc940b319e72434574e052594
Zel9278 <countbaster@gmail.com>
2024-01-09 15:05:41 +0900

    aaa

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use midi_toolkit::{
         to_vec, unwrap_items, TimeCaster,
     },
 };
+use alsa::seq;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -20,6 +21,27 @@ struct Args {
     /// Path of the midi file
     #[arg(short, long)]
     midi_file: String,
+}
+
+const SEQ_ID:   i32 = 14;
+const SEQ_PORT: i32 = 0;
+
+fn note( s: &alsa::Seq, evt : alsa::seq::EventType,  channel : u8, note: u8, velocity : u8, s_port:i32)
+{
+        let o_note = alsa::seq::EvNote {
+                channel,
+                note,
+                velocity,
+                off_velocity: 0,
+                duration: 0
+        };
+        let mut o_event = alsa::seq::Event::new( evt , &o_note );
+
+        o_event.set_direct();
+        o_event.set_source( s_port );
+        o_event.set_dest( alsa::seq::Addr { client: SEQ_ID, port: SEQ_PORT } );
+        let _ = s.event_output( &mut o_event ) ;
+        let _ = s.drain_output();
 }
 
 fn main() {
@@ -66,15 +88,50 @@ fn main() {
         }
 
         if let Some(serialized) = e.as_u32() {
-            let mut chunks: Vec<String> = Vec::new();
-
             for i in (0..24).step_by(8) {
                 let chunk = ((serialized >> i) & 0xFF) as u8;
-                chunks.push(format!("{:02X}", chunk));
-            }
+                let command = chunk >> 4;
+                let channel = chunk & 0x0F;
 
-            let joined = chunks.join(",");
-            println!("{}", joined);
+                if command == 0x09 {
+                    let note_num = ((serialized >> (i + 8)) & 0xFF) as u8;
+                    let velocity = ((serialized >> (i + 16)) & 0xFF) as u8;
+
+                    if velocity == 0 {
+                        note(
+                            &alsa::Seq::open(None, Some(alsa::Direction::Playback), false).unwrap(),
+                            seq::EventType::Noteoff,
+                            channel,
+                            note_num,
+                            velocity,
+                            0
+                        );
+                    }
+
+                    note(
+                        &alsa::Seq::open(None, Some(alsa::Direction::Playback), false).unwrap(),
+                        seq::EventType::Noteon,
+                        channel,
+                        note_num,
+                        velocity,
+                        0
+                    );
+                }
+
+                if command == 0x08 {
+                    let note_num = ((serialized >> (i + 8)) & 0xFF) as u8;
+                    let velocity = ((serialized >> (i + 16)) & 0xFF) as u8;
+
+                    note(
+                        &alsa::Seq::open(None, Some(alsa::Direction::Playback), false).unwrap(),
+                        seq::EventType::Noteoff,
+                        channel,
+                        note_num,
+                        velocity,
+                        0
+                    );
+                }
+            }
         }
     }
     eprintln!("evt_playing_finished");
